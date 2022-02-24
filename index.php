3f375864f9080102c5b8bd8ee95472c2371b54cd
Jcnt98 <jcnt98@gmail.com>
2022-02-24 17:20:45 -0600

    inicio modulo pagos
    Iniciamos la creación del mmodulo de pagos

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     if($pagina=="usuarios") include "assets/pages/usuarios.php";
     if($pagina=="clientes") include "assets/pages/clientes.php";
 	if($pagina=="control") include "assets/pages/control.php";
+	if($pagina=="pagos") include "assets/pages/pagos.php";
 
 	}else{
 		$pagina="login";
