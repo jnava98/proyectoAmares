d3fa1b0b71530e5dcbff2f1d6caa533b0293efa5
jnava <jcnt98@gmail.com>
2022-03-03 10:45:52 -0600

    cambios para en linea

--- assets/php/conexion.php
+++ assets/php/conexion.php
@@ -3,22 +3,6 @@
 
 
 function conectar(){
-	$conexion=mysqli_connect("localhost","root", ""); //LOCAL
-	mysqli_select_db($conexion,"cobranza_amares")or die ("ninguna BD seleccionada");
-	//$conexion=mysqli_connect("us-cdbr-east-05.cleardb.net","b64207390e8bf7","708d1f71"); //HEROKU
-	//mysqli_select_db($conexion,"heroku_3d71280996bd50e")or die ("ninguna BD seleccionada");
-	$conexion->set_charset("utf8");
-	return $conexion;
-}//fin funcion conectar
-
-function desconectar(){
-	mysqli_close(conectar());
-}//fin de funcion desconectar
-
-
-//CONEXION EN LINEA
-/*
-function conectar(){
 	//$conexion=mysqli_connect("localhost","root", ""); //LOCAL
 	//mysqli_select_db($conexion,"cobranza_amares")or die ("ninguna BD seleccionada");
 	$conexion=mysqli_connect("us-cdbr-east-05.cleardb.net","b64207390e8bf7","708d1f71"); //HEROKU
@@ -30,10 +14,6 @@ function conectar(){
 function desconectar(){
 	mysqli_close(conectar());
 }//fin de funcion desconectar
-*/
-
-
-
 
 
 /*
