e0e1affc7c1e836f9eaa9f7025b967ecc328eab2
Jcnt98 <jcnt98@gmail.com>
2022-04-25 20:10:41 -0500

    Update conexion.php

--- assets/php/conexion.php
+++ assets/php/conexion.php
@@ -3,10 +3,10 @@
 
 
 function conectar(){
-	$conexion=mysqli_connect("localhost","root", ""); //LOCAL
-	mysqli_select_db($conexion,"cobranza_amares")or die ("ninguna BD seleccionada");
-	//$conexion=mysqli_connect("us-cdbr-east-05.cleardb.net","b64207390e8bf7","708d1f71"); //HEROKU
-	//mysqli_select_db($conexion,"heroku_3d71280996bd50e")or die ("ninguna BD seleccionada");
+	//$conexion=mysqli_connect("localhost","root", ""); //LOCAL
+	//mysqli_select_db($conexion,"cobranza_amares")or die ("ninguna BD seleccionada");
+	$conexion=mysqli_connect("us-cdbr-east-05.cleardb.net","b64207390e8bf7","708d1f71"); //HEROKU
+	mysqli_select_db($conexion,"heroku_3d71280996bd50e")or die ("ninguna BD seleccionada");
 	$conexion->set_charset("utf8");
 	return $conexion;
 }//fin funcion conectar
