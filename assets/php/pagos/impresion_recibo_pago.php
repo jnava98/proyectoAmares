a5988ce9a99e435b2356aa746cba61f03e7710ac
RobLucci12 <76019902+RobLucci12@users.noreply.github.com>
2022-05-21 14:02:36 -0500

    Recibo de pagos
    Recibo de pagos realizado

--- assets/php/pagos/impresion_recibo_pago.php
+++ assets/php/pagos/impresion_recibo_pago.php
@@ -39,38 +39,32 @@ $fecha_pago = $row['fecha_pago'];
 $Y_Table_Position=18;
     $X_Table_Position=15;
     $pdf->AddPage();
+    $pdf->Image('assets/img/FourCardinalsLogo.png',15,15,40,20);
     $pdf->SetY($Y_Table_Position);
     $pdf->SetX($X_Table_Position);
     $pdf->Image('assets/img/Amares.png',155,15,40,15);
-    $pdf->Line(20, 45, 210-20, 45); // 20mm from each edge
+    //$pdf->Line(20, 45, 210-20, 45); // 20mm from each edge
     $pdf->Ln();
     $pdf->SetY($Y_Table_Position+=20);
     $pdf->SetX($X_Table_Position+=10);
-    $pdf->SetFont('Arial','B',16);
+    $pdf->SetFont('Arial','B',20);
     $pdf->SetTextColor(0,0,0);
 
     $pdf->SetY($Y_Table_Position);
     $pdf->SetX($X_Table_Position);
-    $pdf->MultiCell(180,5,utf8_decode('RECIBO DE PAGO'),0,'J',0);
+    $pdf->MultiCell(160,5,utf8_decode('RECIBO'),0,'C',0);
     $pdf->Ln();
 
-    $pdf->SetFont('Arial','B',12);
+    $pdf->SetFont('Arial','',12);
     $pdf->SetY($Y_Table_Position+=16);
     $pdf->SetX($X_Table_Position-=10);
-    $pdf->MultiCell(180,5,utf8_decode('Nombre de la empresa: Amares Riviera Maya'),0,'J',0);
-    $pdf->Ln();
-
-    $pdf->SetY($Y_Table_Position+=16);
-    $pdf->SetX($X_Table_Position);
     $fecha_aux = date("d/m/Y", strtotime($fecha_pago));
-    $pdf->MultiCell(180,5,utf8_decode('Fecha: '.$fecha_aux),0,'J',0);
+    $pdf->MultiCell(180,5,utf8_decode('Fecha: '.$fecha_aux),0,'R',0);
     $pdf->Ln();
 
-    $pdf->SetY($Y_Table_Position+=16);
+    $pdf->SetY($Y_Table_Position+=10);
     $pdf->SetX($X_Table_Position);
-    $formatter = new NumeroALetras();
-    $valor_letras = $formatter->toMoney($monto_pagado, 2, 'DÓLARES', 'CENTAVOS');
-    $pdf->MultiCell(180,5,utf8_decode('Cantidad pagada: '.$monto_pagado.' '.$valor_letras),0,'J',0);
+    $pdf->MultiCell(180,5,utf8_decode('Playa del Carmen, Solidaridad, Q. Roo'),0,'R',0);
     $pdf->Ln();
 
     $sql="SELECT c.nombre, c.apellido_paterno, c.apellido_materno, co.id_contrato, c.correo, c.direccion, c.telefono from contrato as co inner join cliente_contrato as cc on co.id_contrato=cc.id_contrato inner join clientes as c on c.id_cliente = cc.id_cliente where co.id_contrato like '".$id_contrato."'";
@@ -78,19 +72,16 @@ $Y_Table_Position=18;
     desconectar();
     $num=mysqli_num_rows($result);
     if($num>0){
-        $pdf->SetFont('Arial','B',14);
-        $pdf->SetY($Y_Table_Position+=30);
-        $pdf->SetX($X_Table_Position);
-        $pdf->MultiCell(180,5,utf8_decode('Clientes'),0,'J',0);
-        $pdf->Ln();
+        $aux = 0;
         while($col=mysqli_fetch_array($result)){
-            $pdf->SetY($Y_Table_Position+=15);
-            $pdf->SetX($X_Table_Position);
-            $pdf->SetFont('Arial','B',12);
-            $pdf->MultiCell(180,5,utf8_decode('Nombre del cliente: '.$col['nombre']." ".$col['apellido_paterno']." ".$col['apellido_materno']),0,'J',0);
-            $pdf->Ln();
+            if($aux == 0){
+                $clientes = $col['nombre']." ".$col['apellido_paterno']." ".$col['apellido_materno'];
+                $aux = 1;
+            }else{
+                $clientes.=", ".$col['nombre']." ".$col['apellido_paterno']." ".$col['apellido_materno'];
+            }//fin del else
 
-            $pdf->SetY($Y_Table_Position+=10);
+            /*$pdf->SetY($Y_Table_Position+=10);
             $pdf->SetX($X_Table_Position);
             $pdf->MultiCell(180,5,utf8_decode('Dirección: '.$col['direccion']),0,'J',0);
             $pdf->Ln();
@@ -103,8 +94,67 @@ $Y_Table_Position=18;
             $pdf->SetY($Y_Table_Position+=10);
             $pdf->SetX($X_Table_Position);
             $pdf->MultiCell(180,5,utf8_decode('Teléfono: '.$col['telefono']),0,'J',0);
-            $pdf->Ln();
+            $pdf->Ln();*/
         }//fin del while
+        $pdf->SetFont('Arial','',12);
+        $pdf->SetY($Y_Table_Position+=20);
+        $pdf->SetX($X_Table_Position);
+        $pdf->MultiCell(50,5,utf8_decode('Recibí por parte de:'),0,'J',0);
+        $pdf->Ln();
+        $pdf->SetY($Y_Table_Position);
+        $pdf->SetX($X_Table_Position+=50);
+        $pdf->MultiCell(130,5,utf8_decode(strtoupper($clientes)),0,'J',0);
+        $pdf->Ln();
+
+        $pdf->SetFont('Arial','',12);
+        $pdf->SetY($Y_Table_Position+=20);
+        $pdf->SetX($X_Table_Position-=50);
+        $pdf->MultiCell(50,5,utf8_decode('La cantidad de:'),0,'J',0);
+        $pdf->Ln();
+        $pdf->SetY($Y_Table_Position);
+        $pdf->SetX($X_Table_Position+=50);
+        $formatter = new NumeroALetras();
+        $valor_letras = $formatter->toMoney($monto_pagado, 2, 'DÓLARES', 'CENTAVOS');
+        $pdf->MultiCell(130,5,utf8_decode($monto_pagado.' USD.'),0,'J',0);
+        $pdf->Ln();
+        $pdf->SetY($Y_Table_Position+=10);
+        $pdf->SetX($X_Table_Position);
+        $formatter = new NumeroALetras();
+        $valor_letras = $formatter->toMoney($monto_pagado, 2, 'DÓLARES', 'CENTAVOS');
+        $pdf->MultiCell(130,5,utf8_decode($valor_letras."."),0,'J',0);
+        $pdf->Ln();
+
+        $pdf->SetFont('Arial','',12);
+        $pdf->SetY($Y_Table_Position+=20);
+        $pdf->SetX($X_Table_Position-=50);
+        $pdf->MultiCell(50,5,utf8_decode('En concepto de:'),0,'J',0);
+        $pdf->Ln();
+        $sql="SELECT l.super_manzana, l.mza, l.lote, l.m2 from contrato as c inner join lotes as l on c.id_lote=l.id_lote where c.id_contrato like '".$id_contrato."'";
+        $result=mysqli_query(conectar(),$sql);
+        desconectar();
+        $lote="";
+        $num = mysqli_num_rows($result);
+        if($num>0){
+            $row = mysqli_fetch_array($result);
+            $lote = "Supermanzana ".$row['super_manzana']." Manzana ".$row['mza']." Lote ".$row['lote'];
+        }//fin del if
+        $pdf->SetY($Y_Table_Position);
+        $pdf->SetX($X_Table_Position+=50);
+        $pdf->MultiCell(130,5,utf8_decode($lote),0,'J',0);
+        $pdf->Ln();
+
+        $pdf->SetY($Y_Table_Position+=110);
+        $pdf->SetX($X_Table_Position-=50);
+        $pdf->Image('assets/img/FourCardinals.png',65,180,80,40);
+        $pdf->Line(60, 230, 210-60, 230);
+        $pdf->MultiCell(180,5,utf8_decode('Cesar Gomez Paredes'),0,'C',0);
+        $pdf->Ln();
+        $pdf->SetY($Y_Table_Position+=12);
+        $pdf->SetX($X_Table_Position);
+        $pdf->MultiCell(180,5,utf8_decode('Director Administrativo Amares'),0,'C',0);
+        $pdf->Ln();
     }//fin del if  
+
+    
 
     $pdf->Output();
