6afca459209f42c5ff47d1950a8b274b7c16611e
RobLucci12 <cesartn12@gmail.com>
2022-07-13 12:19:03 -0500

    Panel de control
    Hace falta un manejo de meses para el arreglo

--- assets/php/panel_control/cargar_grafica_lotes_vendidos.php
+++ assets/php/panel_control/cargar_grafica_lotes_vendidos.php
@@ -20,8 +20,8 @@ if($num>0){
         $fecha_uno = date("Y-m-d",strtotime($fecha_uno."- 5 month"));
         $fecha_dos = date("Y-m-d",strtotime($fecha_uno."+ 1 month"));
         for ($i = 0; $i <= 5; $i++) {
-            $sql="SELECT count(*) from contrato as c inner join lotes as l on c.id_lote = l.id_lote inner join cat_tipo_lote as ctl where c.fecha_firma >= '$fecha_uno' && c.fecha_firma < '$fecha_dos' && ctl.id_tipo_lote like '".$col['id_tipo_lote']."'";//Consultar id de la variable
-            echo $sql;
+            $sql="SELECT count(*) from contrato as c inner join lotes as l on c.id_lote = l.id_lote inner join cat_tipo_lote as ctl on l.id_tipo_lote = ctl.id_tipo_lote where c.fecha_firma >= '$fecha_uno' && c.fecha_firma < '$fecha_dos' && ctl.id_tipo_lote like '".$col['id_tipo_lote']."'";//Consultar id de la variable
+            //echo $sql;
             $result_lotes=mysqli_query(conectar(),$sql);
             desconectar();
             $num=mysqli_num_rows($result_lotes);
